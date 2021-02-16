063cc22ea962f8a7e4bf3785938a3f6985cf472d
Dag4 <dul.dagmara@gmail.com>
2021-02-16 18:41:21 +0100

    first user deal

--- src/views/Game/Game.js
+++ src/views/Game/Game.js
@@ -14,31 +14,12 @@ class Game extends React.Component{
     }
     
 
-    
-
-    // shuffleCards = () =>{
-    //     let deckKey = this.stacardsapi.com/api/deck/new/shuffle/?deck_count=6')
-    //     .then(newDeck => newDeck.json())
-    //     // .then(data => console.log(data.deck_id))
-    //     // .then(data => this.setState({deckKey:data.deck_id}))
-    //     // .then(console.log(deckKey))
-    //     .then(data => fetch(`https://deckofcardsapi.com/api/deck/${data.deck_id}/draw/?count=2`) )
-    //     .then(firstRoundCards => firstRoundCards.json())
-    //     .then(da => (this.setState({userCards: [da.cards[1].image]})))
-    //     .then(console.log(this.state.userCards))
-    //     // .then(data => console.log(data.deck_id))
-    //     // .then(fetch('https://deckofcardsapi.com/api/deck/${data.deck_id}/draw/?count=2'))
-    //     // .then(resp => console.log(resp))
-    //     .catch(err => console.log(err))
-    // };
-
     shuffleCards = () =>{
         
         let deckKey = this.state.deckKey;
         
         fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6') //tasowanie kart
         .then(newDeck => {
-            // console.log(newDeck.json())
             return newDeck.json()
         })
         .then(data => {
@@ -56,44 +37,45 @@ class Game extends React.Component{
         .then (data1 =>{
             console.log(data1)
             return this.setState({userCards: data1.cards.map(function(e){return e.image})})
-            // console.log(this.state.userCards)
-            // return this.state.userCards.map(function (card){
-            //     console.log(card)
-            // })
         }).then(singleCard =>{
             console.log(singleCard)
             console.log(this.state.userCards)
             let userCardsInFirstRound = "'" + this.state.userCards[0] + "'";
             console.log(userCardsInFirstRound)
-            const test = document.getElementById('test');
-            console.log(test)
-            var singleCTBox = document.createElement("div")
-            singleCTBox.innerHTML = `<img src=${userCardsInFirstRound} />`
-            // test.innerHTML=`<img src=${userCardsInFirstRound} />`
-            test.appendChild(singleCTBox)
-            // <div className="CardsImages">
-                {/* ${this.state.userCards.map(function(image){
-                    return `<img src=${image} className="singleCardImage"/> `
-                })} */}
+            const userCardsPackage = document.getElementById('userCardsPackage');
+
+            this.state.userCards.map(function(car){
                 
-            // </div>
+                userCardsPackage.insertAdjacentHTML('beforeend', `<div class=${styles.cards}> <img src='${car}' /></div>`);
+                
+            })
             
-            // userCardsInFirstRound.map(function(element){
-            //     userCardPackage.appendChild(element)
+            // this.state.userCards.map(function(card){
+                
+            //     userCardsPackage.appendChild(document.createElement("div").innerHTML=`<img src='${card}' />`)
+            //     console.log(card)
             // })
+
+            // var singleCTBox = document.createElement("div")
+            // singleCTBox.innerHTML = `<img src=${userCardsInFirstRound} />`
+            
+                // test.innerHTML=`<img src=${userCardsInFirstRound} />`
+            
+            // userCardsPackage.appendChild(singleCTBox)
+            
+            
+                // <div className="CardsImages">
+                    {/* ${this.state.userCards.map(function(image){
+                        return `<img src=${image} className="singleCardImage"/> `
+                    })} */}
+                    
+                // </div>
+                
+                // userCardsInFirstRound.map(function(element){
+                //     userCardPackage.appendChild(element)
+                // })
         })
-        // .then(data => console.log(data.deck_id))
-        // .then(data => this.setState({deckKey:data.deck_id}))
-        // .then(console.log(deckKey))
-
-        // .then(data => fetch(`https://deckofcardsapi.com/api/deck/${data.deck_id}/draw/?count=2`) )
-        // .then(firstRoundCards => firstRoundCards.json())
-        // .then(da => (this.setState({userCards: [da.cards[1].image]})))
-        // .then(console.log(this.state.userCards))
-
-        // .then(data => console.log(data.deck_id))
-        // .then(fetch('https://deckofcardsapi.com/api/deck/${data.deck_id}/draw/?count=2'))
-        // .then(resp => console.log(resp))
+        
         .catch(err => console.log(err))
     };
 
@@ -106,7 +88,12 @@ class Game extends React.Component{
             <h1>widok gry</h1>
             <button onClick={() => this.shuffleCards()}>sprawdz karty</button>
             <div className={styles.croupierCardsPackage}>karty krupiera</div>
-            <div className={styles.userCardsPackage} id="test">karty gracza</div>
+            <div className={styles.userCardsPackage} id="userCardsPackage">karty gracza
+                <div id="first"></div>
+                <div id="second"></div>
+                <div id="third"></div>
+                <div id="fourth"></div>
+            </div>
             </>
         )
     }
